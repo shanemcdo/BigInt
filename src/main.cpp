4860930758946674558322773412440ed0d6139b
Shane McDonough <shanemcdman@gmail.com>
2020-07-19 00:20:34 -0700

    Simplify code, less repitition

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,9 @@ int main(){
     std::cout << a << " " << a << std::endl;
     bool b = BigInt(-12222) < BigInt(-12224);
     std::cout << (b ? "true" : "false") << std::endl;
-    b = BigInt(23) > BigInt(23);
+    b = BigInt(22) > BigInt(-23);
     std::cout << (b ? "true" : "false") << std::endl;
-    b = BigInt(-24) <= BigInt(-23);
+    b = BigInt("232323232323232323232323232323232323232") <= BigInt("3232323232323232323232323232323232323232");
     std::cout << (b ? "true" : "false") << std::endl;
     b = BigInt(50) >= BigInt(50);
     std::cout << (b ? "true" : "false") << std::endl;
